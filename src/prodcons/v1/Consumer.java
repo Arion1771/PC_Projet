65378b232a1e140d28ec2cfb9dd27b9c4a507950
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2025-11-06 13:27:49 +0100

    Consumer+Producer

--- src/prodcons/v1/Consumer.java
+++ src/prodcons/v1/Consumer.java
@@ -1,4 +1,31 @@
 package prodcons.v1;
-public class Consumer {
+import prodcons.Message;
+public class Consumer extends Thread {
+    
+    ProdConsBuffer buff;
+    Message msg;
+    int consTime;
+
+    public Consumer (ProdConsBuffer b, int time) {
+        this.buff=b;
+        this.consTime=time;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                msg = this.buff.get();
+            } catch (InterruptedException e) {
+                System.out.println("Thread " + this.getId() + " was interrupted\n");
+            }
+            try {
+                sleep(consTime);
+            } catch (InterruptedException e) {
+                System.out.println("Thread " + this.getId() + " was interrupted while sleeping\n");
+            }
+            System.out.println("Thread " + this.getId() + " received message: " + msg);
+        }
+    }
 
 }
--- src/prodcons/v1/Producer.java
+++ src/prodcons/v1/Producer.java
@@ -1,4 +1,37 @@
 package prodcons.v1;
-public class Producer {
+
+import prodcons.Message;
+
+public class Producer extends Thread {
+    Message mess;
+    ProdConsBuffer buff;
+    int prodTime;
+    int nbMess;
+
+    public Producer (ProdConsBuffer b, int time, int nbmess) {
+        this.buff=b;
+        this.prodTime=time;
+        this.nbMess=nbmess;
+    }
+
+    @Override
+    public void run() {
+        while (nbMess>0) {
+            try {
+                this.buff.put(mess);
+            } catch (InterruptedException e) {
+                System.out.println("Thread "+this.getId()+" was interrupted\n");
+            }
+            try {
+                sleep(prodTime);
+            } catch (InterruptedException e) {
+                System.out.println("Thread "+this.getId()+" was interrupted while sleeping\n");
+            }
+            System.out.println("Thread "+this.getId()+" sent message: "+mess);
+            nbMess--;
+        }
+        
+    }
+    
 
 }
